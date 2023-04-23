78ca0266327b170d30fddda035f09a9152528b70
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-23 18:46:51 +0200

    aggiunto pari e dispari

--- js/script.js
+++ js/script.js
@@ -1,28 +1,59 @@
-const bodY = document.getElementById("body")
-let word = prompt("inserisci parola");
-function IsPalindrome(word){
-    let len = word.length;
-    word.toLowerCase()
-     for(let i = 0; i < len/2; i++){
-      if(word[i] === word[len - 1 - i]){
-        return true
+// const bodY = document.getElementById("body")
+// let word = prompt("inserisci parola");
+// function IsPalindrome(word){
+//     let len = word.length;
+//     word.toLowerCase()
+//      for(let i = 0; i < len/2; i++){
+//       if(word[i] === word[len - 1 - i]){
+//         return true
+//       }
+//       else {
+//         return false
+//     } 
+
+// }
+// }
+
+// let check= IsPalindrome(word)
+// function creatEl() {
+//     const x = document.createElement("h1");
+//     if (check) {
+//         x.innerHTML= "True"
+//     }else {
+//         x.innerHTML= "False"
+//     } 
+//     document.body.appendChild(x);
+
+// }
+// creatEl()
+// pari dispari
+//utente sceglie pari o dispari
+let scelta = prompt("Scegli 2 per pari 1 per dispari");
+// inserisci un numero
+let numero = prompt("Inserisci un numero da 1 a 5")
+//genera un numero ramdom da 1 a 5 
+function random() {
+    let r = Math.floor(Math.random() * 6);
+    return r;
+}
+let r =random();
+console.log(r)
+let somma = somm(r, numero)
+console.log(somma)
+//somma
+function somm(r,numero) {
+    let somma = parseInt(numero) + r;
+    return somma
+}
+function check() {
+    if(somma % 2 == 0 && scelta == 2 || somma % 2 != 0 && scelta == 1){
+        alert("Hai vinto")
       }
-      else {
-        return false
-    } 
-
+      //else if(somma % 2 != 0 && scelta == 1){
+       // alert("Hai vinto")
+//}
+else{
+    alert("Hai perso")
 }
 }
-
-let check= IsPalindrome(word)
-function creatEl() {
-    const x = document.createElement("h1");
-    if (check) {
-        x.innerHTML= "True"
-    }else {
-        x.innerHTML= "False"
-    } 
-    document.body.appendChild(x);
-
-}
-creatEl()
+check()
